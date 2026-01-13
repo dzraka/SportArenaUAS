367d01c26fb8e9d4f3ae39ead6ac0a9e483691da
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-01-13 20:22:46 +0700

    Add RegisterRequest class for handling user registration with validation rules

---
+++ sportArena-api/app/Http/Requests/Auth/registerRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+            'phone_number' => 'required|string|min:10|max:13'
+        ];
+    }
+}
