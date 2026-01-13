d22bdcf6f264b30e3412a7557aa486aef410d8b0
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-01-13 12:26:05 +0700

    Add models, migrations, and API routes for booking system
    - Created Booking and Field models.
    - Added migrations for users, personal access tokens, fields, bookings, and updated the sanctum configuration.
    - Implemented basic API route for user retrieval with Sanctum authentication.
    - Updated composer dependencies for Sanctum and documentation generation.

--- sportArena-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ sportArena-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -17,7 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'customer'])->default('customer');
+            $table->string('phone_number')->nullable();
+            $table->string('profile_photo_path')->nullable();
             $table->timestamps();
         });
 
