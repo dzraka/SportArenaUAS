0ed36c9946b8557da03fb95bdc4e0709981fa3a0
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-01-14 15:32:46 +0700

    Add HttpService class for API communication and manage HTTP requests

---
+++ sportArena/lib/data/service/http_service.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String baseURL = 'http://127.0.0.1:8000/api/';
+
+  String? _token;
+
+  void setToken(String token) {
+    _token = token;
+    log('Token set: $token');
+  }
+
+  void clearToken() {
+    _token = null;
+    log('Token cleared');
+  }
+
+  Map<String, String> _getHeaders({bool isMultipart = false}) {
+    final headers = {'Accept': 'application/json'};
+    if (!isMultipart) {
+      headers['Content-Type'] = 'application/json';
+    }
+    if (_token != null) {
+      headers['Authorization'] = 'Bearer $_token';
+    }
+    return headers;
+  }
+
+  Future<http.Response> get(String endpoint) async {
+    final url = Uri.parse('$baseURL$endpoint');
+    final response = await http.get(url, headers: _getHeaders());
+    log(response.body);
+    return response;
+  }
+
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseURL$endPoint');
+    final response = await http.post(
+      url,
+      headers: _getHeaders(),
+      body: jsonEncode(body),
+    );
+    return response;
+  }
+
+  Future<http.Response> postWithFile(
+    String endPoint,
+    Map<String, String> fields,
+    File? file,
+    String fileFieldName,
+  ) async {
+    try {
+      final url = Uri.parse('$baseURL$endPoint');
+      final request = http.MultipartRequest('POST', url);
+
+      request.headers.addAll(_getHeaders(isMultipart: true));
+      request.fields.addAll(fields);
+
+      if (file != null) {
+        final imageFile = await http.MultipartFile.fromPath(
+          fileFieldName,
+          file.path,
+        );
+        request.files.add(imageFile);
+        log('File added: ${file.path}');
+      }
+      log('POST with File to: $url');
+      log('Fields: ${request.fields}');
+      log('Files: ${request.files.length}');
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+      log('POST with File Response: ${response.statusCode}');
+      return response;
+    } catch (e) {
+      log('Error in postWithFile: $e');
+      rethrow;
+    }
+  }
+
+  Future<http.Response> put(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseURL$endPoint');
+    final response = await http.put(
+      url,
+      headers: _getHeaders(),
+      body: jsonEncode(body),
+    );
+    log('PUT Response: ${response.body}');
+    return response;
+  }
+
+  Future<http.Response> delete(String endPoint) async {
+    final url = Uri.parse('$baseURL$endPoint');
+    final response = await http.delete(url, headers: _getHeaders());
+    return response;
+  }
+}
