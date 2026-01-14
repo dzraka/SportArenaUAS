4fbdeb3a410d44beb1afa8e374d8b3751ba88ee3
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-01-14 23:35:32 +0700

    Fix namespace for LoginRequest and add HasApiTokens trait to User model

--- sportArena-api/app/Http/Controllers/Api/AuthController.php
+++ sportArena-api/app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Http\Requests\Auths\LoginRequest;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
 
--- sportArena-api/app/Http/Requests/Auth/LoginRequest.php
+++ sportArena-api/app/Http/Requests/Auth/LoginRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Auths;
+namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
