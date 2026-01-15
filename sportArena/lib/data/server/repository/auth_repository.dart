01c029e95a4d0f11e593418a91c45a52a4989ac0
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-01-15 08:22:48 +0700

    Implement logout, getUserProfile, and updateProfile methods in AuthRepository

--- sportArena/lib/data/server/repository/auth_repository.dart
+++ sportArena/lib/data/server/repository/auth_repository.dart
@@ -1,5 +1,6 @@
 import 'package:final_project/data/server/usecase/request/login_request.dart';
 import 'package:final_project/data/server/usecase/request/register_request.dart';
+import 'package:final_project/data/server/usecase/response/get_user_response.dart';
 import 'package:final_project/data/server/usecase/response/login_response.dart';
 import 'package:final_project/data/server/usecase/response/register_response.dart';
 import 'package:final_project/data/service/http_service.dart';
@@ -43,18 +44,71 @@ class AuthRepository {
     }
   }
 
+  Future<bool> logout() async {
+    final token = await _getToken();
+
+    try {
+      await httpService.post(
+        'logout',
+        {},
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('auth_token');
+
+      return true;
+    } catch (e) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('auth_token');
+      return false;
+    }
+  }
+
+  Future<GetUserResponse> getUserProfile() async {
+    final token = await _getToken();
+
+    try {
+      final response = await httpService.get(
+        'me',
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      final responseData = GetUserResponse.fromJson(response.body);
+
+      if (response.statusCode == 200) {
+        return responseData;
+      } else {
+        return responseData;
+      }
+    } catch (e) {
+      throw Exception('Get Profle Error: $e');
+    }
+  }
+
+  Future<bool> updateProfile(Map<String, dynamic> data) async {
+    final token = await _getToken();
+
+    try {
+      final response = await httpService.post(
+        'profile',
+        data,
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      return response.statusCode == 200;
+    } catch (e) {
+      throw Exception('Update Profle Error: $e');
+    }
+  }
+
   Future<void> _saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('auth_token', token);
   }
 
-  Future<String?> getToken() async {
+  Future<String?> _getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('auth_token');
-  }
-
-  Future<void> logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove('auth_token');
   }
 }
